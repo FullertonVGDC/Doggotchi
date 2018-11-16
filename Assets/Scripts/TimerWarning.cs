5c7f46834048ea13ffe102b3da2f58d987290924
William <williamau10@gmail.com>
2018-11-16 15:41:46 -0800

    Make branch

---
+++ Assets/Scripts/TimerWarning.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerWarning : MonoBehaviour
+{
+	private Image warning;
+	[SerializeField]
+	private TimerText time;
+	private Animator anim;
+	// Use this for initialization
+	void Start ()
+	{
+		warning = GetComponent<Image>();
+		anim = GetComponent<Animator>();
+	}
+	
+	void FixedUpdate ()
+	{
+		if (time.timeLeftInt == 30)
+		{
+			anim.SetBool("warning", true);
+		}
+		else
+		{
+			anim.SetBool("warning", false);
+		}
+	}
+}
