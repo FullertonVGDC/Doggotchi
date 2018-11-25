1acb25060ad9a927b01964d6ced9af0d94936fa7
Kristi Chang <kristi.chang@csu.fullerton.edu>
2018-11-25 13:39:51 -0800

    Missing files fixed, somewhat organized

---
+++ Assets/_Scripts/ButtonBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehavior : MonoBehaviour
+{
+ 
+    public Texture2D cursor;
+
+    void Start()
+    {
+        cursor = (Texture2D)Resources.Load("cursor.jpg");
+    }
+
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+
+}
