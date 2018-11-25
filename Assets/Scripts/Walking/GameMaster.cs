37d8b63015ca4dcf1470e28afd46c3b2bba1861a
Kristi Chang <kristi.chang@csu.fullerton.edu>
2018-11-25 12:31:40 -0800

    Organized Files

---
+++ Assets/Scripts/Walking/GameMaster.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+	private GameObject player;
+	// Use this for initialization
+	void Start ()
+	{
+		player = GameObject.Find("Doggo");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+	private void FixedUpdate()
+	{
+		FindIfFellOff();
+	}
+	private void FindIfFellOff()
+	{
+		if (player.transform.position.y < -10f)
+		{
+			GameOver();
+		}
+	}
+	private void GameOver()
+	{
+
+	}
+}
