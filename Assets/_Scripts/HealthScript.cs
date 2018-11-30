22f57be59a06cd65293acbe576ebb69f78c2fbde
Justin Huang <justin_huang@csu.fullerton.edu>
2018-11-30 03:50:14 -0800

    Add files via upload
    BaseStatsScript

---
+++ Assets/_Scripts/HealthScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour
+{
+    public Image Bar;
+    public float max_health = 100f;
+    public float cur_health = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        cur_health = max_health;
+        InvokeRepeating("decreaseHealth", 0f, 2f);
+
+    }
+
+    void decreaseHealth()
+    {
+        cur_health -= 10f;
+        float calc_health = cur_health / max_health;
+        SetHealth(calc_health);
+    }
+
+    void SetHealth(float myhealth)
+    {
+        Bar.fillAmount = myhealth;
+    }
+}
+	
