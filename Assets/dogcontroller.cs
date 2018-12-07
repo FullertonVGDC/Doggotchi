45bde15399aa575a302e15caadb628acb5b6ba6b
Kristi Chang <kristi.chang@csu.fullerton.edu>
2018-12-07 12:51:18 -0800

    credits and home

---
+++ Assets/dogcontroller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dogcontroller : MonoBehaviour {
+
+    public GameObject sit;
+    public GameObject bark;
+
+	// Use this for initialization
+	void Start () {
+        sit.SetActive(true);
+        bark.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButton(0)) {
+            sit.SetActive(false);
+            bark.SetActive(true);
+        }
+        else
+        {
+            sit.SetActive(true);
+            bark.SetActive(false);
+        }
+    }
+}
