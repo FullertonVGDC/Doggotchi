c7bcf52f7cbb11d328f752898542604c2f0bba79
Tara <taropaca@gmail.com>
2018-12-08 15:21:52 -0800

    Added BGM to games so they do not destroy on load

---
+++ Assets/MusicClass.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+ using UnityEngine;
+ 
+ public class MusicClass : MonoBehaviour
+ {
+     private AudioSource _audioSource;
+     private void Awake()
+     {
+         DontDestroyOnLoad(this);
+         _audioSource = GetComponent<AudioSource>();
+
+         if (FindObjectsOfType(GetType()).Length > 1)
+         {
+             Destroy(gameObject);
+         }
+     }
+ 
+     public void PlayMusic()
+     {
+         if (_audioSource.isPlaying) return;
+         _audioSource.Play();
+     }
+ 
+     public void StopMusic()
+     {
+         _audioSource.Stop();
+     }
+ }
